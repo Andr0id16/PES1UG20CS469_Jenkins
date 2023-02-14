ca4127ae3049d39d33e85dd1737a4ed65ebf6776
Andr0id16 <pracheth.thamankar@gmail.com>
2023-02-14 13:57:03 +0530

    Added Jenkins-main

---
+++ main/hello.cpp
@@ -0,0 +1,10 @@
+//File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World from PES1UG20CS469\n";
+  cout << "Hello, Jenkins from PES1UG20CS469\n";
+  cout << "I have successfully built and run\n";
+  return 0;
+}
